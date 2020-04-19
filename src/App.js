08544bd42b48fdab5fa83f7442d46e4ffe1d2e30
antonio <jesus.shilon@enova.mx>
2020-04-18 20:22:38 -0500

    Feature: props and state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Person name="Max" age="12"/>
-      <Person name="Manu" age="29">I like soccer</Person>
-      <Person name="Louis" age="32"/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 23},
+      {name: 'Manu', age: 24},
+      {name: 'Stephany', age: 26},
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I like soccer</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
 }
 
 export default App;
